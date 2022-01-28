3635e959c829e38798ca63f0b0bf5f3fc31817fb
İsmail Sezer Bullalıoğlu <bullalioglu@gmail.com>
2022-01-28 21:22:45 +0300

    Set theme to dark between 6 pm and 6 am. Update CSS to conform with this change.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let currentDate = new Date();
+let currentTime = currentDate.getHours();
+
+if(currentTime >= 18 || currentTime < 6) {
+    document.body.classList.add("dark-theme");
+}
+
 document.querySelector(".toggle-theme").addEventListener("click", function() {
     document.body.classList.toggle("dark-theme");
 });
